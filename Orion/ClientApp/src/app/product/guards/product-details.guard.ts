c6f0dff64f6b6cf1680c57eadabe845a9f49e023
Steff Beckers <steff@steffbeckers.com>
2025-05-31 15:46:12 +0200

    ng update @angular/core@16 @angular/cli@16

--- Orion/ClientApp/src/app/product/guards/product-details.guard.ts
+++ Orion/ClientApp/src/app/product/guards/product-details.guard.ts
@@ -1,12 +1,12 @@
 import { Injectable } from '@angular/core';
-import { ActivatedRouteSnapshot, CanActivate, RouterStateSnapshot, UrlTree } from '@angular/router';
+import { ActivatedRouteSnapshot, RouterStateSnapshot, UrlTree } from '@angular/router';
 import { Observable } from 'rxjs';
 import {ProductFacade} from "../store/product.facade";
 
 @Injectable({
   providedIn: 'root'
 })
-export class ProductDetailsGuard implements CanActivate {
+export class ProductDetailsGuard  {
 
   constructor(private productFacade: ProductFacade) {
   }
